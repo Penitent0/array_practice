ea7767977109bd55fc36264f41c269c7b85202ae
Ken Lenhart <omnipaid@gmail.com>
2022-06-08 10:26:27 -0700

    Update arrays.rb with index positions

--- arrays.rb
+++ arrays.rb
@@ -11,13 +11,13 @@ sleepy = [true, false, true, false]
 
 flowers.push("orchid", "carnation")
 
-# The .pop method will "pop" or remove the last element of the numbers array
+# The .pop method will "pop" or remove the last element 4 of the numbers array
 # and return just the removed element.
 
 numbers.pop
 
 # The .shift(2) method will remove the first two elements of the prices array
-# and return just the removed elements. The remaining elements in the array will
+# and return just the removed elements 1.99 and 2.50. The remaining elements in the array will
 # be moved down in order.
 
 prices.shift(2)
@@ -27,3 +27,20 @@ prices.shift(2)
 # the entire array.
 
 sleepy.unshift(false)
+
+# Index Positions and new methods: When the each_index method is called on the flowers array
+# the method will return the given element (++) and the index position of each
+# element within the array.
+
+flowers.each_index {|x| print x, " ++ "}
+
+# Index Positions: This will return the first (index 0) element "false" in the
+# sleep array.
+
+sleepy[0]
+
+# Index Positions: This will return the second (index 1) and third (index 2)
+# elements 2 and 3 of the numbers array.
+
+
+numbers[1, 2]
