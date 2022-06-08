af9c48996bb95e363b1301c6d349aa13ab8f10be
Ken Lenhart <omnipaid@gmail.com>
2022-06-08 09:56:05 -0700

    Update array.rb with methods

--- arrays.rb
+++ arrays.rb
@@ -5,3 +5,25 @@ numbers = [1, 2, 3, 4]
 prices = [1.99, 2.50, 0.50, 3.75]
 
 sleepy = [true, false, true, false]
+
+# The .push method will append the "orchid" and "carnation" elements to the
+# end of flowers array and return the entire array.
+
+flowers.push("orchid", "carnation")
+
+# The .pop method will "pop" or remove the last element of the numbers array
+# and return just the removed element.
+
+numbers.pop
+
+# The .shift(2) method will remove the first two elements of the prices array
+# and return just the removed elements. The remaining elements in the array will
+# be moved down in order.
+
+prices.shift(2)
+
+# The .unshift method will prepend the "false" element to the front of the sleepy
+# array, moving the other elements in the array up in order, and return the
+# the entire array.
+
+sleepy.unshift(false)
